ff3499ddca7ed52968553b28d5bc2ff26ddb4c70
Оксана <o.baydakova@yandex.ru>
2023-01-24 17:26:43 +0300

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,53 +10,33 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        File inputFile = new File("C:\\Users\\Oksana\\Desktop\\input.txt");
-        File outputFile = new File("C:\\Users\\Oksana\\Desktop\\output.txt");
-        try (Scanner sc = new Scanner(inputFile);
-             FileWriter fw = new FileWriter(outputFile, false);) {
-
-            String str = sc.nextLine();
-            String[] words = str.split(" ");
-            double a = 0, b = 0;
+        File file = new File("C:\\Users\\Oksana\\Desktop\\input.txt");
+        Scanner sc1 = new Scanner(file);
+        Scanner sc2 = new Scanner(file);
+        FileWriter fw = new FileWriter("C:\\Users\\Oksana\\Desktop\\output.txt");
+        while (sc1.hasNext()) {
             try {
-                a = Double.parseDouble(words[0]);
-                b = Double.parseDouble(words[2]);
-            } catch (NumberFormatException e) {
-                fw.write("Error! Not number");
+                double a = sc1.nextDouble();
+                String op = sc1.next();
+                double b = sc1.nextDouble();
+                String ops = "+-*/";
+                String result = "";
+                if (!ops.contains(op)) result = "Operation Error!";
+                switch (op) {
+                    case "+": result = Double.toString(a + b); break;
+                    case "-": result = Double.toString(a - b); break;
+                    case "*": result = Double.toString(a * b); break;
+                    case "/": result = (b == 0 ? "Error! Division by zero" : Double.toString(a / b)); break;
+                }
+                fw.append(sc2.nextLine() + " = " + result + "\n");
                 fw.flush();
-                return;
-            }
-            try {
-                if (words[1].equals("+")) {
-                    fw.write(String.valueOf(a + b));
-                    fw.flush();
-                    return;
-                }
-                if (words[1].equals("-")) {
-                    fw.write(String.valueOf(a - b));
-                    fw.flush();
-                    return;
-                }
-                if (words[1].equals("*")) {
-                    fw.write(String.valueOf(a * b));
-                    fw.flush();
-                    return;
-                }
-                if (words[1].equals("/")) {
-                    if (b == 0d) throw new ArithmeticException("Error! Division by zero");
-                    fw.write(String.valueOf(a / b));
-                    fw.flush();
-                    return;
-                }
-                fw.write("Operation Error!");
+            } catch (Exception ex) {
+                fw.append(sc2.nextLine() + " = " + "Error! Not number" + "\n");
                 fw.flush();
-
-            } catch (ArithmeticException e) {
-                fw.write(e.getMessage());
-                fw.flush();
-                return;
+                sc1.nextLine();
             }
         }
+        fw.close(); sc1.close(); sc2.close();
 
 
     }
